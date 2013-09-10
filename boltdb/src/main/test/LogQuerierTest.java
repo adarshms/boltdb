db1486f0534791c5464d3de7870320901ed39377
Adarsh <adarsh.mysore.s@gmail.com>
2013-09-09 21:25:19 -0500

    Adding test cases

---
+++ boltdb/src/main/test/LogQuerierTest.java
@@ -0,0 +1,19 @@
+import junit.framework.TestCase;
+
+
+public class LogQuerierTest extends TestCase {
+	
+	@Test
+	public void test
+
+}
+
+
+//TODO : Push log files to servers
+
+//TODO : Run command line dgrep with options 
+//TODO : Read output of dgrep
+//TODO : Run grep on local log files and generate output
+//TODO : Compare two outputs
+
+
