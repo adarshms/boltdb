7b7d075553f397612415e20fed646d334c0bf053
Ashwin <ashwin@ashwin-VirtualBox.(none)>
2013-09-12 00:36:17 -0500

    Grouping output of client

--- boltdb/src/main/java/edu/uiuc/boltdb/logquerier/client/LogQuerierClient.java
+++ boltdb/src/main/java/edu/uiuc/boltdb/logquerier/client/LogQuerierClient.java
@@ -1,15 +1,16 @@
 package edu.uiuc.boltdb.logquerier.client;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
-import java.net.ConnectException;
 import java.net.InetAddress;
-import java.net.Socket;
+import java.util.LinkedList;
 import java.util.Properties;
+
+import org.apache.tools.ant.taskdefs.optional.clearcase.ClearCase;
 
 import edu.uiuc.boltdb.logquerier.utils.ClientArgs;
 
@@ -26,34 +27,64 @@ public class LogQuerierClient {
 		FileInputStream fis = new FileInputStream("./boltdb.prop");
 		prop.load(fis);
 		fis.close();
-		System.out.println();
-
 		ClientArgs clientArgs = new ClientArgs();
 		// TODO error cases needed;check for spaces,incorrect option
+		StringBuilder options = new StringBuilder();
 		for (int j = 0; j < args.length; j++) {
 			if (args[j].equals("-key")) {
 				clientArgs.setKeyRegExp(args[++j]);
 			} else if (args[j].equals("-value")) {
 				clientArgs.setValRegExp(args[++j]);
-			} else if(args[j].startsWith("-") || args[j].startsWith("--")) {
-				clientArgs.addOption(args[j]);
+			} else  {
+				options.append(args[j]);
+				options.append(" ");
 			}
 		}
-
+		clientArgs.addOption(options.toString());
 		if (clientArgs.getKeyRegExp().isEmpty()
 				&& clientArgs.getValRegExp().isEmpty()) {
-			throw new IllegalArgumentException();
+			System.out.println("Error : Both key and value missing");
 		}
 
 		String[] addresses = prop.getProperty("machines.address").split(",");
+		LinkedList<Thread> clientThreads = new LinkedList<Thread>();
 		for (int i = 0; i < addresses.length; i++) {
 			String[] hostPort = addresses[i].split(":");
 			// TODO error conditions
 			InetAddress address = InetAddress.getByName(hostPort[0]);
-			Thread clientThread = new LogQuerierClientThread(address,
+			Thread newThread = new LogQuerierClientThread(address,
 					Integer.parseInt(hostPort[1]), clientArgs);
-			clientThread.start();
+			newThread.start();
+			clientThreads.add(newThread);
+		}
+		int completedThreads = 0;
+		while(completedThreads != addresses.length) {
+			for(int i = 0; i < clientThreads.size(); i++) {
+				if(!clientThreads.get(i).isAlive()) {
+					completedThreads++;
+					printOutput(clientThreads.get(i));
+					clientThreads.remove(i);
+				}
+			}
 		}
 
+	}
+	
+	private static void printOutput(Thread t) throws IOException {
+		LogQuerierClientThread lct = (LogQuerierClientThread) t;
+		File outputFile = new File("output-"+lct.getAddress().getHostAddress()+"."+lct.getPort());
+		if(outputFile.exists()) {
+			System.out.println();
+			BufferedReader br = new BufferedReader(new FileReader(outputFile));
+			String line;
+			System.out.println("Logs from "+ lct.getAddress()+":"+lct.getPort());
+			while((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			br.close();
+			outputFile.delete();
+		} /*else {
+			System.out.println(outputFile+" doesnt exists");
+		}*/
 	}
 }
--- boltdb/src/main/java/edu/uiuc/boltdb/logquerier/client/LogQuerierClientThread.java
+++ boltdb/src/main/java/edu/uiuc/boltdb/logquerier/client/LogQuerierClientThread.java
@@ -1,9 +1,11 @@
 package edu.uiuc.boltdb.logquerier.client;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -26,11 +28,14 @@ public class LogQuerierClientThread extends Thread {
 		try {
 			connection = new Socket(address, port);
 		} catch (ConnectException ce) {
+			System.out.println();
 			System.out.println(address.getHostAddress() + ":" + port
 					+ " is not reachable");
 			System.out.println();
+			return;
 		} catch (IOException ioe) {
 			System.out.println("IOException:"+ioe.getMessage() +"occurred while connecting to "+address.getHostAddress()+":"+port);
+			return;
 		}
 		try {
 		ObjectOutputStream outToServer = new ObjectOutputStream(
@@ -41,19 +46,26 @@ public class LogQuerierClientThread extends Thread {
 	
 		outToServer.writeObject(clientArgs);
 		String line;
-		//System.out.println("Logs from " + address.getHostAddress()+":"+port);
-		//System.out
-		//		.println("-------------------------------------------------");
-		while ((line = inFromServer.readLine()) != null)
-			System.out.println("["+address.getHostAddress()+":"+port+"]"+line);
-
-		System.out.println();
-		System.out.println();
+	
+		PrintWriter writer = new PrintWriter("output-"+address.getHostAddress()+"."+port, "UTF-8");
+		while ((line = inFromServer.readLine()) != null) {
+			writer.println(line);	
+		}
+		writer.close();
 		connection.close();
 		} catch(IOException ioe) {
 			System.out.println("IOException:"+ioe.getMessage() +"occurred while getting data from "+address.getHostAddress()+":"+port);
 		}
 	}
 
+	public InetAddress getAddress() {
+		return address;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	
 }
 
