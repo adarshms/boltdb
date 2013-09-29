6520e8695717b1e4228af61afec1faf859b14413
Ashwin Shankar <ashwinshankar77@gmail.com>
2013-09-29 11:58:07 -0500

    adding code for receive gossip

--- boltdb/src/main/java/edu/uiuc/boltdb/groupmembership/ReceiveGossipThread.java
+++ boltdb/src/main/java/edu/uiuc/boltdb/groupmembership/ReceiveGossipThread.java
@@ -1,5 +1,34 @@
 package edu.uiuc.boltdb.groupmembership;
 
-public class ReceiveGossipThread {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class ReceiveGossipThread implements Runnable {
+
+	private ServerSocket serverSocket;
+	private int gossipPort = 8888;
+	public ReceiveGossipThread() throws IOException {
+		serverSocket = new ServerSocket(gossipPort);
+	}
+	@Override
+	public void run() {
+			
+		while (true) {
+			try {
+				Socket clientSocket = serverSocket.accept();
+				MergeThread mergeThread = new MergeThread(clientSocket);
+				//TODO think of failure scenarios here
+				new Thread(mergeThread).start();
+			} catch (IOException e) {
+				System.out.println("Problem accepting client connection.");
+			}
+			
+			
+			
+		}
+		
+	}
+	
+	
 }
